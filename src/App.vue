39ea612564dbf22dec5e1e2d4530f7312a71a98f
JulianChen <julian9925@gmail.com>
2016-06-09 17:50:44 +0800

    set window layout and add menu and ment-item component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <window></window>
+  <window>
+    <tree-menu>
+      <tree-menu-item is-expanded text="test"></tree-menu-item>
+      <tree-menu-item is-expanded text="Hello"></tree-menu-item></tree-menu-item>
+    </tree-menu>
+  </window>
 </template>
 
 <script>
 require('materialize-css/dist/css/materialize.min.css');
 require('material-design-icons/iconfont/material-icons.css');
 import window from './components/window.vue';
+import TreeMenu from './components/TreeMenu.vue';
+import TreeMenuItem from './components/TreeMenuItem.vue';
 
 export default {
   components: {
     window,
+    TreeMenu,
+    TreeMenuItem,
+  },
+  data: () => ({
+    currentView: '',
+  }),
+  events: {
+    listenChange(view) {
+      this.currentView = view;
+      this.$broadcast('changeview', this.currentView);
+    },
   },
 };
 </script>
