ee589e47c4785cb258eb96f1cae87ddfee117540
Julian Chen <julius.j@inwinstack.com>
2016-06-13 17:03:59 +0800

    seprate tree-menu item from window to BackupPage

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <window></window>
+  <window>
+    <backup-page></backup-page>
+  </window>
 </template>
 
 <script>
 require('materialize-css/dist/css/materialize.min.css');
 require('material-design-icons/iconfont/material-icons.css');
 import window from './components/window.vue';
+import BackupPage from './components/BackupPage.vue';
 
 export default {
   components: {
     window,
+    BackupPage,
   },
   data: () => ({
     currentView: '',
---
+++ src/components/BackupPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="row" id="app">
+    <div class="col-md-3">
+      <tree-menu>
+        <tree-menu-item isExpanded view="BackupPlan" icon="view_agenda" title="Buliding Up Backup Plan"></tree-menu-item>
+        <tree-menu-item isExpanded view="TaskList" icon="format_list_bulleted" title="List Backup Tasks"></tree-menu-item>
+      </tree-menu>
+    </div>
+    <div class="col-md-9">
+      <component :is="currentView"></component>
+    </div>
+  </div>
+</template>
+
+<style>
+h1 {
+  color: #42b983;
+}
+</style>
+
+<script>
+import TreeMenu from './TreeMenu.vue';
+import TreeMenuItem from './TreeMenuItem.vue';
+import TaskList from './TaskList.vue';
+import BackupPlan from './BackupPlan.vue';
+
+export default {
+  components: {
+    TreeMenu,
+    TreeMenuItem,
+    TaskList,
+    BackupPlan,
+  },
+  data() {
+    return {
+      currentView: 'BackupPlan',
+    };
+  },
+  events: {
+    changeView(view) {
+      this.currentView = view;
+    },
+  },
+};
+</script>
+
