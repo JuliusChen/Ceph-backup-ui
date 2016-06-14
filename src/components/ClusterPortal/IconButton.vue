714446c87804a509ec4f2ca992fb5f103bd038ae
DesmondS <desmond.s@inwinstack.com>
2016-06-14 17:30:44 +0800

    remove the redundent comment
    Signed-off-by: DesmondS <desmond.s@inwinstack.com>

--- src/components/ClusterPortal/IconButton.vue
+++ src/components/ClusterPortal/IconButton.vue
@@ -1,9 +1,4 @@
 <template>
-  <!--
-  <div class="waves-effect waves-light btn-large green lighten-4 grey-text text-darken-3">
-    <i class="material-icons left">{{ icon }}</i>{{ subject }}
-  </div>
-  -->
   <div class="icon-button green lighten-5 grey-text text-darken-3">
     <i class="material-icons left medium">{{ icon }}</i>
     <div style="padding-top:20px;">{{ subject }}</div>
