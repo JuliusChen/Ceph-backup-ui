4fa5bc7f3f4bebefbe75b2933b407610b12188fe
Julian Chen <julius.j@inwinstack.com>
2016-06-15 14:28:56 +0800

    rename ambiguous variable and adjust form input field CSS

--- src/components/BackupPlan.vue
+++ src/components/BackupPlan.vue
@@ -3,18 +3,18 @@
     <form class="col s12">
       <input value="RBD" type="radio" id="radio1" v-model="picked"/>
       <label for="radio1">RBD(block device)</label><br>
-      <div v-if="state.isRBD">
-        <input value="yes" type="radio" id="radio2" v-model="selected"/>
-        <label for="radio2">Backup RBD Pools Image</label><br>
+      <div v-if="state.isRBD" class="tab">
+        <input value="RBDPool" type="radio" id="radio2" v-model="selected"/>
+        <label for="radio2">Backup RBD Pools</label><br>
         <div v-if="state.useRBDPool">
 
-          <div v-for="pool in RBDpoolLists">
+          <div v-for="pool in RBDpoolLists" class="tab">
             <input value="{{ pool }}" type="checkbox" id="{{ pool }}" v-model="submit.selectedRBDPools"/>
             <label for="{{ pool }}">{{ pool }}</label><br>
           </div>
         </div>
 
-        <input value="no" type="radio" id="radio3" v-model="selected"/>
+        <input value="RBDImage" type="radio" id="radio3" v-model="selected"/>
         <label for="radio3">Backup Selected Pool and Specified Images</label><br>
           <div v-if="state.useRBDImage">
             <select class="browser-default" v-model="submit.selectedRBDPool">
@@ -30,12 +30,18 @@
                 </div>
               </div>
             </div>
+            <div>
+              <label for="backupCount">Incremental backup counts between full backup</label>
+              <input type="number" placeholder="Pick a number" v-model="submit.backupNumberCount" id="backupCount" min="0" max="100">
+              <label for="backupIteraion">Preserve backup iteration count</label>
+              <input type="number" placeholder="Pick a number" v-model="submit.backupInteration" id="backupIteraion" min="0" max="100">
+            </div>
           </div>
       </div>
 
       <input value="RADOS" type="radio" id="radio4" v-model="picked"/>
       <label for="radio4">RADOS</label><br>  
-      <div v-if="state.isRADOS">
+      <div v-if="state.isRADOS" class="tab">
         <div class="FixedHeightContainer">
           <span>Selected Backup pools</span>
           <div class="Content">
@@ -62,13 +68,21 @@ h1 {
   height:250px;
   width:100%; 
   padding:3px; 
-    background:#a9a;
+  background:#a9a;
 }
 .Content
 {
   height:224px;
-   overflow:auto;
-    background:#fff;
+  overflow:auto;
+  background:#fff;
+}
+.tab
+{
+  padding-left: 2em;
+}
+.tab2
+{
+  padding-left: 4em;
 }
 </style>
 
@@ -89,6 +103,8 @@ export default {
         selectedRBDPool: '',
         selectedRADOSPools: [],
         selectedimages: [],
+        backupNumberCount: '',
+        backupIteration: '',
       },
       RBDpoolLists: ['RBD pool 0', 'RBD pool 1', 'RBD pool 2'],
       RADOSpoolists: ['.rgw.root', '.rgw.control', '.rgw.gc', '.rgw.buckets', '.rgw.index',
@@ -112,18 +128,22 @@ export default {
         this.state.isRBD = false;
         this.submit.selectedRBDPools = [];
         this.submit.selectedimages = [];
+        this.backupNumberCount = '';
+        this.backupIteration = '';
       }
     },
     selected(val) {
-      if (val === 'yes') {
+      if (val === 'RBDPool') {
         this.state.useRBDPool = true;
         this.state.useRBDImage = false;
         this.submit.selectedimages = [];
         this.submit.selectedRBDPool = '';
-      } else if (val === 'no') {
+      } else if (val === 'RBDImage') {
         this.state.useRBDImage = true;
         this.state.useRBDPool = false;
         this.submit.selectedRBDPools = [];
+        this.backupNumberCount = '';
+        this.backupIteration = '';
       }
     },
   },
