969f84cc910b1d4da562dc0d8ba57f23930abbff
Julian Chen <julius.j@inwinstack.com>
2016-06-15 15:43:07 +0800

    Add ajax hhr service component

---
+++ src/components/XhrService.vue
@@ -0,0 +1,34 @@
+<template>
+</template>
+
+<style>
+</style>
+
+<script>
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
+export default({
+  data() {
+    return {
+      log: 'yes',
+    };
+  },
+  methods: {
+    getPool() {
+      return Vue.http({
+        url: '/test',
+        method: 'GET',
+      }).then((res) => {
+        console.log('Impossible');
+        console.log(res);
+      }, (res) => {
+        console.log('WTF');
+        console.log(res);
+      });
+    },
+  },
+});
+</script>
