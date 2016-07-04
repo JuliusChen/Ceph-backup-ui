511448724205ec4bf1a16e2874353b10128175d6
Julian Chen <julius.j@inwinstack.com>
2016-07-04 16:32:52 +0800

    Add modify

--- test/unit/specs/BackupPlan.spec.js
+++ test/unit/specs/BackupPlan.spec.js
@@ -8,6 +8,9 @@ describe('BackupPlan.vue', () => {
   }).$mount();
 
   it('Backup Plan should render correct contents', () => {
-    
+    const menuItems = vm.$el.querySelectorAll('div.tree-menu-item');
+    expect(menuItems[0].textContent).to.contain('Ceph Cluster 1');
+    expect(menuItems[1].textContent).to.contain('Buliding Up Backup Plan');
+    expect(menuItems[2].textContent).to.contain('List Backup Tasks');
   });
 });
